e3d6fb1139d1d7daf5e34e933ce89fab31668323
maximebert <maximebertinelli@gmail.com>
2022-02-25 12:05:38 +0100

    ready to start

--- back/app/routers/index.js
+++ back/app/routers/index.js
@@ -1,5 +1,18 @@
 const express = require('express');
 
+const userRouter = require('./user');
+const itinaryRouter = require('./itinary');
+const motorbikeRouter = require('./motorbike');
+const pictureRouter = require('./picture');
+
 const router = express.Router();
+
+router.use('/', userRouter.find);
+
+router.use('/', itinaryRouter);
+
+router.use('/', motorbikeRouter);
+
+router.use('/', pictureRouter);
 
 module.exports = router;
