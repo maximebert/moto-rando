9ab0d48cd8ce263daa4f7ea3a3648dde9eae2f73
cotton-elodie <elodie.cotton@hotmail.com>
2022-02-25 15:42:15 +0100

    sprint1 createPage

---
+++ front/src/Pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+// import react
+import React from 'react'
+import PropTypes from "prop-types";
+// import style
+import '../Home/home.scss'
+
+// page d'accueil du site
+const Home =()=>{
+return ()
+}
+
+Home.propType={}
+
+export default React.memo(Home)
---
+++ front/src/Pages/Registration/Registration.jsx
@@ -0,0 +1,15 @@
+// import react
+import React from 'react'
+import PropTypes from "prop-types";
+// import style
+import '../Registration/registration'
+
+// page d'un itinéraire
+
+const Registration =()=>{
+
+    return ()
+}
+Registration.PropTypes={}
+
+export default React.memo (Registration);
