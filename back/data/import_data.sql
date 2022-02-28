90fc4309f8da59a3f2109738675ff40a2ceb3335
Karine-Bauch <karine.bauch.web@gmail.com>
2022-02-28 13:03:25 +0100

    Sprint1-seeding OK

---
+++ back/data/import_data.sql
@@ -0,0 +1,19 @@
+BEGIN;
+
+
+INSERT INTO "user" ("alias", "email", "password", "presentation")
+VALUES
+
+
+INSERT INTO "itinary" ("title", "description", "duration", "highway", "kilometer", "curve", "user_id")
+VALUES
+
+
+INSERT INTO  "motorbike" ("brand", "model", "description", "user_id")
+VALUES
+
+INSERT INTO "picture" ("title", "description", "link", "user_id", "motorbike_id", "itinary_id")
+VALUES
+
+
+COMMIT;
