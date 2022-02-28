881cb872003a0a2129a85e7d4af1e8fca47408aa
Albangh <90769567+Albangh@users.noreply.github.com>
2022-02-28 16:32:13 +0100

    sprint-1 create form login

--- front/src/Components/Login/Login.jsx
+++ front/src/Components/Login/Login.jsx
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
+import './login.scss';
 
-const Login = (props) => {
+const Login = ({mailValue, passwordValue}) => {
+    const [buttonDisable, setButtonDisable] = useState(false)
+    const onToggleBtn = () => {
+        setButtonDisable((a) => !a)
+    }
+
     return (
-        <h1>Connexion</h1>
+        <div className='login'>
+            <button className='form__btn' onClick={onToggleBtn}>Connexion</button>
+            { buttonDisable &&
+                <form className='form'>
+                    <input
+                        className='form__input'
+                        placeholder='email'
+                        type='email'
+                    />
+                    <input
+                        className='form__input'
+                        placeholder='mot de passe'
+                        type='password'
+                    />
+                    <button className='form__btn-submit'>Se connecter</button>
+                </form>
+            }
+        </div>
     )
 }
 
