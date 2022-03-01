85515595b254b0948bd032b58239f2b41afbab87
maximebert <maximebertinelli@gmail.com>
2022-03-01 13:38:08 +0100

    spint1 bcrypt init

--- back/app/controllers/userController.js
+++ back/app/controllers/userController.js
@@ -1,18 +1,41 @@
+const bcrypt = require('bcrypt');
+const emailValidator = require('email-validator');
 const userMapper = require('../models/user');
 const { ApiError } = require('../helpers/errorHandler');
 
 const userController = {
-
-  //   async connection(req, res) {
-
-  //   },
-
   async inscription(req, res) {
-    const newUser = req.body;
-    const user = await userMapper.create(newUser);
-    return res.json(user).status(200);
-  },
+    try {
+      const newAlias = req.body.alias;
+      const user = await userMapper.findByAlias(newAlias);
+      if (user) {
+        return res.json('Cet alias est déjà utilisé par un utilisateur.').status(500);
+      }
+      // - 2: format d'email valide
+      if (!emailValidator.validate(req.body.email)) {
+        return res.render('signup', {
+          error: "Cet email n'est pas valide.",
+        });
+      }
+      // - 3: le mdp et la confirmation ne correspondent pas
+      if (req.body.password !== req.body.confirmPassword) {
+        return res.render('signup', {
+          error: 'La confirmation du mot de passe ne correspond pas.',
+        });
+      }
+      // 5 - On crypt
+      const salt = await bcrypt.genSalt(10);
+      const encryptedPassword = await bcrypt.hash(req.body.password, salt);
 
+      const { alias, email, presentation } = req.body;
+      const hashedPassword = encryptedPassword;
+
+      const newUser = await userMapper.create(alias, email, hashedPassword, presentation);
+      return res.status(201).json(newUser);
+    } catch (err) {
+      return res.status(500).send(err.message);
+    }
+  },
   async findOne(req, res) {
     const { id } = req.params;
     const user = await userMapper.findByPk(id);
