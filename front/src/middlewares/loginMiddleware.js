7a64589ac487e09e367f9465ef33e01fb7aacc10
cotton-elodie <elodie.cotton@hotmail.com>
2022-03-02 16:12:24 +0100

    sprint1 logout

--- front/src/middlewares/loginMiddleware.js
+++ front/src/middlewares/loginMiddleware.js
@@ -1,23 +1,28 @@
-import {actionSetLogged, SUBMIT_LOGIN} from "../action/user";
-import {requestLogin} from "../request/loginRequest";
+import {
+  actionSetLogged,
+  actionsSetPseudo,
+  SUBMIT_LOGIN,
+} from "../action/user";
+import { requestLogin } from "../request/loginRequest";
 
 const loginMiddleware = (store) => (next) => async (action) => {
-    switch (action.type) {
-        case SUBMIT_LOGIN: {
+  switch (action.type) {
+    case SUBMIT_LOGIN: {
+      const { user } = store.getState();
+      const response = await requestLogin(user.email, user.password);
 
-            const { user } = store.getState();
-            const response = await requestLogin(user.email, user.password);
-
-            if (response.status === 200) {
-                store.dispatch(actionSetLogged(true))
-            } else {
-                store.dispatch(actionSetLogged(false))
-            }
-            return;
-        }
-        default:
-            next(action);
+      if (response.status === 200) {
+        store.dispatch(actionSetLogged(true));
+        store.dispatch(actionsSetPseudo(response.alias));
+      } else {
+        store.dispatch(actionSetLogged(false));
+      }
+      return;
     }
-}
+
+    default:
+      next(action);
+  }
+};
 
 export default loginMiddleware;
