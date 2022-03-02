07f174f7a76ea23d3e8d28a8c76081f95f93276d
YannJoseph <jvc7710@yahoo.com>
2022-03-02 14:30:43 +0100

    heroku final

--- back/app/models/database.js
+++ back/app/models/database.js
@@ -1,2 +1,11 @@
 const { Pool } = require('pg');
+
+if (process.env.NODE_ENV === 'production') {
+  config.connectionString = process.env.DATABASE_URL;
+  // ici, on désactive l'obligation pour notre app node de se
+  // connecter à la BDD en ssl (mode sécurisé)
+  config.ssl = {
+      rejectUnauthorized: false,
+  };
+};
 module.exports = new Pool();
