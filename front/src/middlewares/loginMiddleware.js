899b5bb25015c459d28a3b57198a6671712687bb
cotton-elodie <elodie.cotton@hotmail.com>
2022-03-02 17:02:19 +0100

    sprint1 add comments

--- front/src/middlewares/loginMiddleware.js
+++ front/src/middlewares/loginMiddleware.js
@@ -5,12 +5,14 @@ import {
 } from "../action/user";
 import { requestLogin } from "../request/loginRequest";
 
+//loginMiddleware  va capturer la requete login
 const loginMiddleware = (store) => (next) => async (action) => {
+  // si l'action SUBMIT_LOGIN est récupéré on fait la requete
   switch (action.type) {
     case SUBMIT_LOGIN: {
       const { user } = store.getState();
       const response = await requestLogin(user.email, user.password);
-
+      // si le status de la reponse est bien = à 200 alors l'utilisateur est connecté et on affiche son pseudo
       if (response.status === 200) {
         store.dispatch(actionSetLogged(true));
         store.dispatch(actionsSetPseudo(response.alias));
